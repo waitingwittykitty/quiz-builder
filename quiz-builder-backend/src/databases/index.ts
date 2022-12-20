4c0234b7837a425d6020d49e6863a714f4d232b4
arthur delossantos <arthur@github.com>
2022-12-20 11:38:12 -0600

    feat<be>: add DB_URL to env

--- quiz-builder-backend/src/databases/index.ts
+++ quiz-builder-backend/src/databases/index.ts
@@ -1,7 +1,7 @@
-import { DB_HOST, DB_PORT, DB_DATABASE } from '@config';
+import { DB_HOST, DB_PORT, DB_DATABASE, DB_URL } from '@config';
 
 export const dbConnection = {
-  url: `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
+  url: DB_URL || `mongodb://${DB_HOST}:${DB_PORT}/${DB_DATABASE}`,
   options: {
     useNewUrlParser: true,
     directConnection: true,
